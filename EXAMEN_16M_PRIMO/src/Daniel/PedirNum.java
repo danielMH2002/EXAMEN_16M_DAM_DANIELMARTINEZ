2824bd3abf617eb75c7db4109e27f166d2fd79e6
danielMH2002 <167062455+danielMH2002@users.noreply.github.com>
2024-05-22 12:10:21 +0200

    asd

---
+++ EXAMEN_16M_PRIMO/src/Daniel/PedirNum.java
@@ -0,0 +1,16 @@
+package Daniel;
+
+import java.util.Scanner;
+
+public class PedirNum {
+public static void main(String[] args) {
+	Scanner ent = new Scanner(System.in);
+	String num="";
+	System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+	
+	System.out.println("Introduce un número para averiguar si es primo o no");
+	num = ent.nextLine();
+	System.out.println("tu numero es" + num);
+
+}
+}
